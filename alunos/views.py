4d533d39e17e10270112bb20a4e05a44458173b5
Victor Fernandes <victor.fernandes@rededor.com.br>
2021-09-28 02:30:15 -0300

    Ajustes nos alunos

--- alunos/views.py
+++ alunos/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from alunos.models import Aluno
 # Create your views here.
 
 @login_required(login_url='/autenticacao/login')
 def dashboard(request):
     current_user = request.user
-    print(current_user.email)
+    aluno = Aluno.objects.get(email=current_user.email)
+    colegas_classe = Aluno.objects.filter(sala=aluno.sala)
+    print(colegas_classe)
     return render(request, 'alunos/dashboard.html')
 
 @login_required(login_url='/autenticacao/login')
 def informacao_aluno(request):
-    return render(request, 'alunos/informacao.html')
+    current_user = request.user
+    aluno = Aluno.objects.get(email=current_user.email)
+    print(aluno)
+    return render(request, 'alunos/informacao.html', { aluno: aluno })
 
 
